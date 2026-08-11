2ede3145e685ddfae039c26ef705012015edd098
eduardomachadoferla <166259033+eduardomachadoferla@users.noreply.github.com>
2026-08-10 21:20:12 -0300

    Aula 2

---
+++ ativ1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){//classe principal
+	int idade;//declarando uma variavel
+	
+	printf("digite sua idade");//imprime uma mensagem na tela
+	scanf("%d", &idade);//captura uma variavel
+	
+	if (idade >= 18){// estrutura de decsao
+	printf("voce ja pode tirara carteira de motorista");//imprme uma mensagem na tela
+}
+	
+	printf("fim do programa");//imprmne uma mensagem na tela 
+	
+	return 0;//fim do programa 
+	
+	}
+
